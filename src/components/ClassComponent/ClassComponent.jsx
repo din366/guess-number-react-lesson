4d989427975cfce2f191c7ef84163acfcfe5857d
Alex Din <din366@gmail.com>
2022-05-24 00:27:58 +0300

    add lesson_5.3

--- src/components/ClassComponent/ClassComponent.jsx
+++ src/components/ClassComponent/ClassComponent.jsx
@@ -1,19 +1,96 @@
 import React from 'react';
 import style from './ClassComponent.module.css';
+import PropTypes from 'prop-types';
 
 export class ClassComponent extends React.Component {
+  state = {
+    result: 'Результат',
+    userNumber: '',
+    randomNumber:
+      Math.floor(Math.random() * this.props.max - this.props.min) +
+      this.props.min,
+    count: 0,
+    again: '',
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState(state => ({
+      count: state.count + 1,
+    }));
+
+    this.setState(state => {
+      if (!state.userNumber) {
+        return {
+          result: `Введите число`,
+        };
+      }
+
+      if (state.userNumber > state.randomNumber) {
+        return {
+          result: `${state.userNumber} больше загаданного`,
+        };
+      }
+
+      if (state.userNumber < state.randomNumber) {
+        return {
+          result: `${state.userNumber} меньше загаданного`,
+        };
+      }
+
+      return {
+        result: `Вы угадали, загаданное число ${state.userNumber},
+        попыток ${state.count}`,
+        again: <button type='button'
+          className={style.btn} onClick={this.getAgain}>Сыграть еще?</button>,
+      };
+    });
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      userNumber: e.target.value,
+    });
+  };
+
+  guessClick = (e) => {
+    e.preventDefault;
+    e.target.closest('form').querySelector(`.${style.input}`)
+      .value = '';
+  };
+
+  getAgain = () => {
+    this.setState({
+      count: 0,
+      again: '',
+      result: 'Результат',
+      randomNumber:
+        Math.floor(Math.random() * this.props.max - this.props.min) +
+        this.props.min,
+    });
+  };
+
   render() {
+    console.log('render');
     return (
       <div className={style.game}>
-        <p className={style.result}>5</p>
-        <form className={style.form}>
+        <p className={style.result}>{this.state.result}</p>
+        <form className={style.form} onSubmit={this.handleSubmit}>
           <label className={style.label} htmlFor='user_number'>
             Угадай число
           </label>
-          <input className={style.input} type='number' id='user_number' />
-          <button className={style.btn}>Угадать</button>
+          <input className={style.input} type='number' id='user_number'
+            onChange={this.handleChange} />
+          <button className={style.btn}
+            onClick={this.guessClick}>Угадать</button>
+          {this.state.again}
         </form>
       </div>
     );
   }
 }
+
+ClassComponent.propTypes = {
+  min: PropTypes.number,
+  max: PropTypes.number,
+};
